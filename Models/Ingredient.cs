de73abd6879c297d952c99408032bed1171907be
ST10252746 <st10252746@vcconnect.edu.za>
2024-06-09 22:38:15 +0200

    Added Student Information to my Model Classes:
    /*
     Theshara Narain - ST10252746
     PROG6221
     BCAD GROUP 1
     POE Part 3
     Github Link for Part 1: https://github.com/VCDN-2024/prog6221-part-1-ST10252746.git
     Github Link for Part 2: https://github.com/VCDN-2024/prog6221-part-2-ST10252746.git
     Github Link for Part 3: https://github.com/ST10252746/prog6221-part-3-ST10252746.git
     */

--- Models/Ingredient.cs
+++ Models/Ingredient.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+ Theshara Narain - ST10252746
+ PROG6221
+ BCAD GROUP 1
+ POE Part 3
+ Github Link for Part 1: https://github.com/VCDN-2024/prog6221-part-1-ST10252746.git 
+ Github Link for Part 2: https://github.com/VCDN-2024/prog6221-part-2-ST10252746.git 
+ Github Link for Part 3: https://github.com/ST10252746/prog6221-part-3-ST10252746.git 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
